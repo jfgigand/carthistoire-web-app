fe92ed579a48b4289fe1639dc9a8b53094e13301
okapi <okapi@a80fe19f-c1ec-4b56-a503-cf2ee58ae7cd>
2010-09-24 07:57:27 +0000

    ign
    git-svn-id: http://code.lapatate.org/svn/trunk/sites/cartapatate-ign@562 a80fe19f-c1ec-4b56-a503-cf2ee58ae7cd

--- src/ign/IgnBundle/Resources/web/js/ign/info/About.js
+++ src/ign/IgnBundle/Resources/web/js/ign/info/About.js
@@ -5,6 +5,7 @@ dojo.require('jig.layout._Anchor');
 dojo.require('dijit._Templated');
 dojo.require('jig.version');
 dojo.require('dijit.layout.AccordionContainer');
+dojo.require('jig.tool.UserFeedback');
 
 // we need to put it somewhere...
 dojo.require('ign.settings');
@@ -20,19 +21,27 @@ dojo.declare('ign.info.About', [ jig.layout._Anchor, dijit._Templated ],
     this.version = jig.version;
   },
 
-  startup: function() {
+  /*startup: function() {
     console.log('startup', this, arguments);
     this.inherited(arguments);
     this.acc.startup();
+  },*/
+
+  sendFeedback: function() {
+    console.log('sendFeedback', this, arguments);
+    var widget = jig.workspace.loadWidget(null,
+      function(id) { return new jig.tool.UserFeedback; });
+    jig.workspace.autoAnchorWidget(widget);
+    widget.startup();
   },
 
   resize: function() {
-    console.log('resize', this, arguments);
+    //console.log('resize', this, arguments);
     try {
       this.acc.resize.apply(this.acc, arguments);
     }
     catch(e) {
-      console.error('exception in  resize', e);
+      //console.error('exception in  resize', e);
     }
   }
 
