0c5351983d73b7526c7fb017a0d7d963bb26f7b1
okapi <okapi@a80fe19f-c1ec-4b56-a503-cf2ee58ae7cd>
2010-09-24 06:01:05 +0000

    ign
    git-svn-id: http://code.lapatate.org/svn/trunk/sites/cartapatate-ign@559 a80fe19f-c1ec-4b56-a503-cf2ee58ae7cd

--- src/ign/IgnBundle/Resources/web/js/ign/info/About.js
+++ src/ign/IgnBundle/Resources/web/js/ign/info/About.js
@@ -4,6 +4,7 @@ dojo.provide('ign.info.About');
 dojo.require('jig.layout._Anchor');
 dojo.require('dijit._Templated');
 dojo.require('jig.version');
+dojo.require('dijit.layout.AccordionContainer');
 
 // we need to put it somewhere...
 dojo.require('ign.settings');
@@ -17,6 +18,22 @@ dojo.declare('ign.info.About', [ jig.layout._Anchor, dijit._Templated ],
 
   postMixInProperties: function() {
     this.version = jig.version;
+  },
+
+  startup: function() {
+    console.log('startup', this, arguments);
+    this.inherited(arguments);
+    this.acc.startup();
+  },
+
+  resize: function() {
+    console.log('resize', this, arguments);
+    try {
+      this.acc.resize.apply(this.acc, arguments);
+    }
+    catch(e) {
+      console.error('exception in  resize', e);
+    }
   }
 
 
